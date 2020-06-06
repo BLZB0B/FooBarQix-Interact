8018c7c5ab1d86413573c36b3f38a03e0a85d948
Philip Yardley <philipyardley@Philips-MacBook-Pro.local>
2020-06-06 01:12:54 +0100

    Step 1 commit

---
+++ FooBarQixKata-Interact-Tests/CalculateTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using FooBarQixKataInteract;
+using NUnit.Framework;
+
+namespace FooBarQixKataInteractTests
+{
+    [TestFixture]
+    public class CalculateTests
+    {
+        /*
+        If the number is divisible by 3, write “Foo” instead of the number
+        If the number is divisible by 5, add “Bar”
+        If the number is divisible by 7, add “Qix”
+        For each digit 3, 5, 7, add “Foo”, “Bar”, “Qix” in the digits order.
+        */
+
+        private FooBarCalculator _fooBarCalculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fooBarCalculator = new FooBarCalculator();
+        }
+
+        public class MyDataClass
+        {
+            public static IEnumerable TestCases
+            {
+                get
+                {
+                    yield return new TestCaseData("1").Returns("1");
+                    yield return new TestCaseData("2").Returns("2");
+                    yield return new TestCaseData("3").Returns("FooFoo");
+                    yield return new TestCaseData("4").Returns("4");
+                    yield return new TestCaseData("5").Returns("BarBar");
+                    yield return new TestCaseData("6").Returns("Foo");
+                    yield return new TestCaseData("7").Returns("QixQix");
+                    yield return new TestCaseData("8").Returns("8");
+                    yield return new TestCaseData("9").Returns("Foo");
+                    yield return new TestCaseData("10").Returns("Bar");
+                    yield return new TestCaseData("13").Returns("Foo");
+                    yield return new TestCaseData("15").Returns("FooBarBar");
+                    yield return new TestCaseData("21").Returns("FooQix");
+                    yield return new TestCaseData("33").Returns("FooFooFoo");
+                    yield return new TestCaseData("51").Returns("FooBar");
+                    yield return new TestCaseData("53").Returns("BarFoo");
+                }
+            }
+        }
+
+        [TestCaseSource(typeof(MyDataClass), "TestCases")]
+        public string Tests(string input)
+        {
+            return _fooBarCalculator.Compute(input);
+
+        }
+
+    }
+}
---
+++ FooBarQixKata-Interact/FooBarCalculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace FooBarQixKataInteract
+{
+    public class FooBarCalculator
+    {
+        /*
+        If the number is divisible by 3, write “Foo” instead of the number
+        If the number is divisible by 5, add “Bar”
+        If the number is divisible by 7, add “Qix”
+        For each digit 3, 5, 7, add “Foo”, “Bar”, “Qix” in the digits order.
+         */
+        public string Compute(string input)
+        {
+
+            var result = ReplaceByDivisors(input);
+            result += ReplaceDigits(input);
+
+            if (string.IsNullOrEmpty(result))
+            {
+                result = input;
+            }
+
+            return result;
+        }
+
+        private string ReplaceDigits(string input)
+        {
+            var result = "";
+
+            foreach (char number in input)
+            {
+                switch (number)
+                {
+                    case '3':
+                        result += "Foo";
+                        break;
+                    case '5':
+                        result += "Bar";
+                        break;
+                    case '7':
+                        result += "Qix";
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        private string ReplaceByDivisors(string input)
+        {
+            var result = "";
+            var number = Convert.ToInt16(input);
+
+            if (number % 3 == 0)
+            {
+                result += "Foo";
+            }
+
+            if (number % 5 == 0)
+            {
+                result += "Bar";
+            }
+
+            if (number % 7 == 0)
+            {
+                result += "Qix";
+            }
+
+            return result;
+        }
+    }
+}
